8908c57dc7b78c3c4d768861fb6618ec0e08c2c5
Lucas <jlucasmelob558@gmail.com>
2026-05-08 00:12:19 -0300

    refactor(api): remove unnecessary response model abstractions

--- apps/server/src/api/controllers/dtos/responses/models/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file index.ts
- * @description Re-exports all response models.
- * @author Lucas
- * @license MIT
- */
-
-export { PrivateUserModel } from './users/PrivateUserModel';
-export { PublicUserModel } from './users/PublicUserModel';
