c4f9cdbbddd031d8935472c4613ff5037e36d7d6
Lucas <jlucasmelob558@gmail.com>
2026-05-10 02:21:10 -0300

    feat(user): scaffold admin and self-management endpoints

--- apps/server/src/api/controllers/UserController.ts
+++ apps/server/src/api/controllers/UserController.ts
@@ -1,16 +1,17 @@
 /**
  * @file UserController.ts
- * @description Controller responsible for user resource endpoints.
- * Requires authentication on all routes.
+ * @description Controller responsible for user resource endpoints. Requires authentication on all routes.
  * @author Lucas
  * @license MIT
  */
 
-import { JsonController, CurrentUser, Authorized, Get, Req } from 'routing-controllers';
+import { JsonController, CurrentUser, Authorized, Get, Req, Patch, Body, Delete } from 'routing-controllers';
+import { UserProfileDTO } from './dtos/responses/UserResponses';
 import { UserService } from '../services/UserService';
 import { BaseController } from './BaseController';
 import { UserEntity } from '@/database/entities';
 import type { Request } from 'express';
+import { UserRole } from '@astra/core';
 import { Service } from 'typedi';
 
 @Service()
@@ -26,6 +27,28 @@ export class UserController extends BaseController {
         @Req() req: Request,
         @CurrentUser() user: UserEntity
     ) {
-        return this.ok(req, user);
+        return this.ok<UserProfileDTO>(req, UserProfileDTO.fromEntity(user));
     }
+
+    @Patch('/@me')
+    public async updateMe() {}
+
+    @Delete('/@me')
+    public async deleteMe() {}
+
+    @Authorized(UserRole.ADMIN)
+    @Get('/:id')
+    public async getUser() {}
+
+    @Authorized(UserRole.ADMIN)
+    @Get('/')
+    public async getUsers() {}
+    
+    @Authorized(UserRole.ADMIN)
+    @Patch('/:id')
+    public async updateUser() {}
+
+    @Authorized(UserRole.ADMIN)
+    @Delete('/:id')
+    public async deleteUser() {}
 }
