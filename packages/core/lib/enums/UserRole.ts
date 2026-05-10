43546c506900f7368906a2e5e1bcfd6c029b276e
Lucas <jlucasmelob558@gmail.com>
2026-05-10 02:25:23 -0300

    feat(api): add user role enum

---
+++ packages/core/lib/enums/UserRole.ts
@@ -0,0 +1,11 @@
+/**
+ * @file UserRole.ts
+ * @description Enum representing the possible roles a user can have within the system.
+ * @author Lucas
+ * @license MIT
+ */
+
+export enum UserRole {
+    ADMIN = 'admin',
+    USER  = 'user'
+}
