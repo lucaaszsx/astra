af782b45d0d71c731de89578a926fb58a69886aa
Lucas <jlucasmelob558@gmail.com>
2026-05-10 00:57:27 -0300

    fix(database): remove stale RoleEntity reference after role system removal

--- apps/server/src/database/repositories/index.ts
+++ apps/server/src/database/repositories/index.ts
@@ -8,13 +8,11 @@
 import { VerificationCodeEntity } from '../entities/user/VerificationCodeEntity';
 import { RefreshTokenEntity } from '../entities/user/RefreshTokenEntity';
 import { SessionEntity } from '../entities/user/SessionEntity';
-import { RoleEntity } from '../entities/user/RoleEntity';
 import { UserEntity } from '../entities/user/UserEntity';
 import { appDataSource } from '../AppDataSource';
 
 /** User */
 export const userRepository = appDataSource.getRepository(UserEntity);
-export const roleRepository = appDataSource.getRepository(RoleEntity);
 export const sessionRepository = appDataSource.getRepository(SessionEntity);
 export const refreshTokenRepository = appDataSource.getRepository(RefreshTokenEntity);
 export const codeRepository = appDataSource.getRepository(VerificationCodeEntity);
